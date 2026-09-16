44e2ecdb3c528092cfa3422790a76ba6180ed3d4
easyddr <dmitro050707@gmail.com>
2026-09-16 22:09:06 +0300

    lab1

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -1,13 +1,20 @@
 package com.example.demo;
 
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class DemoApplication {
+public class DemoApplication implements CommandLineRunner {
 
     public static void main(String[] args) {
+        System.out.println("Hello World");
         SpringApplication.run(DemoApplication.class, args);
+        System.out.println("End of application");
     }
 
+    @Override
+    public void run(String... args) throws Exception {
+        System.out.println("CommandLineRunner");
+    }
 }
